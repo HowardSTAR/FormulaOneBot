e74281690570e51ce231e6358b5ae6d0e0363802
sevag <sevagrigoryev@gmail.com>
2025-11-19 16:24:34 +0300

    добавлены новые команды для обработки

--- app/main.py
+++ app/main.py
@@ -4,11 +4,20 @@ from aiogram.exceptions import TelegramBadRequest
 
 from app.bot import create_bot_and_dispatcher
 from app.handlers.start import router as start_router
+from app.handlers.drivers import router as drivers_router
+from app.handlers.teams import router as teams_router
+from app.handlers.races import router as races_router
+
 
 
 async def main() -> None:
     bot, dp = create_bot_and_dispatcher()
+
+    # Регистрируем все роутеры
     dp.include_router(start_router)
+    dp.include_router(races_router)
+    dp.include_router(drivers_router)
+    dp.include_router(teams_router)
 
     try:
         await dp.start_polling(bot)
