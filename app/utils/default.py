3b19f3ac75ec5b07776fea5942a24aa88446e624
vislavdis <sevagrigoryev@gmail.com>
2025-11-28 09:13:11 +0300

    дефолтные переменные

---
+++ app/utils/default.py
@@ -0,0 +1,42 @@
+# Соответствие кода пилота имени файла с его фотографией
+# Файлы лежат в app/assets/pilots
+DRIVER_CODE_TO_FILE = {
+    "ALB": "Alexander Albon.png",
+    "ANT": "Andrea Kimi Antonelli.png",
+    "SAI": "Carlos Sainz.png",
+    "LEC": "Charles Leclerc.png",
+    "OCO": "Esteban Ocon.png",
+    "ALO": "Fernando Alonso.png",
+    "COL": "Franco Colapinto.png",
+    "BOR": "Gabriel Bortoleto.png",
+    "RUS": "George Russell.png",
+    "HAD": "Isack Hadjar.png",
+    "DOO": "Jack Doohan.png",
+    "STR": "Lance Stroll.png",
+    "NOR": "Lando Norris.png",
+    "HAM": "Lewis Hamilton.png",
+    "LAW": "Liam Lawson.png",
+    "VER": "Max Verstappen.png",
+    "HUL": "Nico Hülkenberg.png",
+    "BEA": "Oliver Bearman.png",
+    "PIA": "Oscar Piastri.png",
+    "GAS": "Pierre Gasly.png",
+    "TSU": "Yuki Tsunoda.png",
+}
+
+
+SESSION_NAME_RU = {
+    "Practice 1": "Практика 1",
+    "Practice 2": "Практика 2",
+    "Practice 3": "Практика 3",
+    "Free Practice 1": "Практика 1",
+    "Free Practice 2": "Практика 2",
+    "Free Practice 3": "Практика 3",
+
+    "Sprint Qualifying": "Спринт-квалификация",
+    "Sprint Shootout": "Спринт-квалификация",  # на всякий случай
+    "Sprint": "Спринт",
+
+    "Qualifying": "Квалификация",
+    "Race": "Гонка",
+}
