ea1e74ad12b1135b2a7e2f4f21c193bf5de476a4
vislavdis <sevagrigoryev@gmail.com>
2026-07-24 11:00:22 +0300

    убран импорт который не используется

--- app/main.py
+++ app/main.py
@@ -20,7 +20,6 @@ from app.utils.backup import create_backup
 from app.utils.notifications import (
     check_and_send_notifications,
     check_and_send_results,
-    check_and_notify_quali,
     check_and_notify_voting_results,
 )
 from app.services.prediction_notifications import check_and_notify_predictions
